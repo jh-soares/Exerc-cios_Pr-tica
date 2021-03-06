090ff7d29fca1b0591ccf2316e457a5e54fd76d7
jh-soares <78885065+jh-soares@users.noreply.github.com>
2021-03-06 15:36:13 -0300

    Add files via upload

---
+++ Fibonacci/src/fibonacci/Fibonacci.java
@@ -0,0 +1,19 @@
+package fibonacci;
+
+public class Fibonacci {
+
+    public static void main(String[] args) {
+        
+        int j = 1;
+        
+        for(int i = 0, cont = 0; cont < 15; cont++){
+            
+            System.out.println(i + " ");
+            
+            i = i + j;
+            j = i - j;
+    
+        }
+    }
+    
+}
